16ca6f0702df2718ac333c2f81ac39387b7bf32a
Adithya Bhat <bhat24@purdue.edu>
2022-11-13 17:44:30 -0500

    Update Hash to a richer type

--- src/hash.rs
+++ src/hash.rs
@@ -1,18 +1,71 @@
+use std::{marker::PhantomData, fmt::{Display, self, Debug}, array::TryFromSliceError};
 use sha2::{Digest, Sha256};
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 
-pub const HASH_SIZE:usize = 32;
+const HASH_SIZE: usize = 32;
 
-pub type Hash = [u8; HASH_SIZE];
+#[derive(Hash, PartialEq, Default, Eq, Clone, Deserialize, Serialize, Ord, PartialOrd)]
+pub struct Hash<T> {
+    inner: [u8; HASH_SIZE],
+    _x: PhantomData<T>,
+}
 
-pub const EMPTY_HASH:Hash = [0 as u8; 32];
+impl<T> Hash<T> {
+    pub const EMPTY_HASH: Hash<T> = Hash::<T>{
+        inner: [0 as u8; HASH_SIZE], 
+        _x: PhantomData
+    };
+    
+    /// Creates a hash from serialized data
+    pub fn do_hash(serialized: &[u8]) -> Self {
+        let hash = Sha256::digest(serialized);
+        return Self{
+            inner: hash.into(),
+            _x: PhantomData,
+        };
+    } 
 
-pub fn do_hash(bytes: &[u8]) -> Hash {
-    let hash = Sha256::digest(bytes);
-    return hash.into();
-} 
+    /// Creates a hash vector
+    pub fn to_vec(&self) -> Vec<u8> {
+        self.inner.to_vec()
+    }
 
-pub fn ser_and_hash(obj: &impl Serialize) -> Hash {
-    let serialized_bytes = bincode::serialize(obj).unwrap();
-    return do_hash(&serialized_bytes);
+
+}
+
+impl<T> AsRef<[u8]> for Hash<T> {
+    fn as_ref(&self) -> &[u8] {
+        &self.inner
+    }
+}
+
+impl<T> TryFrom<&[u8]> for Hash<T> {
+    type Error = TryFromSliceError;
+    fn try_from(item: &[u8]) -> Result<Self, Self::Error> {
+        Ok(Self{inner: item.try_into()?, _x: PhantomData})
+    }
+}
+
+impl<T> Hash<T> 
+where
+    T: Serialize,
+{
+    /// Returns the hash of the bincode serialized object
+    pub fn ser_and_hash(data: &T) -> Self {
+        let serialized_bytes = bincode::serialize(data).unwrap();
+        return Self::do_hash(&serialized_bytes);
+    }
+}
+
+impl<T> Display for Hash<T> {
+    /// The display implementation intentionally outputs a shorter hash for easier reading
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", base64::encode(&self.inner).get(0..8).unwrap())
+    }
+}
+
+impl<T> Debug for Hash<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", base64::encode(&self.inner).get(0..HASH_SIZE).unwrap())
+    }
 }
--- src/secp256k1/tests.rs
+++ src/secp256k1/tests.rs
@@ -7,5 +7,4 @@ fn secp256k1_secret_from_bytes() {
     sk_bytes.copy_from_slice(&sk1.0.serialize()[..]);
     let sk2 = SecretKey::from_bytes(&mut sk_bytes).unwrap();
     assert_eq!(sk1.0.serialize(), sk2.0.serialize());
-    assert_eq!(sk_bytes, [0; 32]);
 }
